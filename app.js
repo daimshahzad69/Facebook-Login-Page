eaad8ef0cf3ddfe9a1c949da290c01739b99a0ac
Daim Shahzad <127994956+daimshahzad69@users.noreply.github.com>
2023-05-24 13:46:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    console.log("Login Details:");
+    console.log("Email or Phone:", email);
+    console.log("Password:", password);
+  }
+  
+  function signup() {
+    var signupEmail = document.getElementById("signup-email").value;
+    var signupPassword = document.getElementById("signup-password").value;
+    console.log("Sign Up Details:");
+    console.log("Email:", signupEmail);
+    console.log("Password:", signupPassword);
+  }
+  
